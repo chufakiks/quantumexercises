9f5303cc11430c250fa22409efb85f962788920e
Lucia Alexandra Little <73139437+chufakiks@users.noreply.github.com>
2026-02-02 15:37:04 +0100

    add phase values and observables

--- cshsexercise.py
+++ cshsexercise.py
@@ -23,3 +23,18 @@ chsh_circuit.cx(0, 1)
 chsh_circuit.ry(theta, 0)
 fig = chsh_circuit.draw(output="mpl", idle_wires=False, style="iqp")
 fig.savefig("circuit.png")
+
+number_of_phases = 21
+phases = np.linspace(0, 2 * np.pi, number_of_phases)
+# Phases need to be expressed as list of lists in order to work
+individual_phases = [[ph] for ph in phases]
+
+# <CHSH1> = <AB> - <Ab> + <aB> + <ab> -> <ZZ> - <ZX> + <XZ> + <XX>
+observable1 = SparsePauliOp.from_list(
+    [("ZZ", 1), ("ZX", -1), ("XZ", 1), ("XX", 1)]
+)
+ 
+# <CHSH2> = <AB> + <Ab> - <aB> + <ab> -> <ZZ> + <ZX> - <XZ> + <XX>
+observable2 = SparsePauliOp.from_list(
+    [("ZZ", 1), ("ZX", 1), ("XZ", -1), ("XX", 1)]
+)
